e4fdf9e47998610829cbf83884e8e1df89bd66e3
zikrya <77081809+zikrya@users.noreply.github.com>
2023-08-20 19:53:29 -0400

    [styling] Updated design for events index

--- app/routes/events._index.tsx
+++ app/routes/events._index.tsx
@@ -19,33 +19,38 @@ export default function Events() {
   const { events } = useLoaderData<typeof loader>();
 
   return (
-    <div className="blue-back min-h-screen">
+    <div className="relative min-h-screen overflow-hidden bg-gray-900">
+      {/* Gradient Background */}
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
+      </div>
+
+      {/* Content */}
       <div className="p-4">
         <h1 className="mb-4 text-2xl text-white">Events</h1>
         <div className="grid grid-cols-3 gap-4">
           {events.map((event) => (
             <Link
-              to={`/events/${event.id}`}
-              key={event.id}
-              className="block rounded border bg-white p-4 shadow"
+                to={`/events/${event.id}`}
+                key={event.id}
+                className="relative block rounded border border-black p-4 shadow-md hover:shadow-xl transform hover:scale-105 transition-transform duration-300 h-60 bg-cover bg-center rounded-md"
+                style={{ backgroundImage: `url('/image-nightlife.jpg')` }}
             >
-              <h2 className="text-xl">{event.title}</h2>
-              <p>{event.description}</p>
-              {/* List tickets and prices */}
-              {event.tickets.map((ticket) => (
-                <div key={ticket.id}>
-                  <p>
-                    <strong>Ticket ID:</strong> {ticket.id}
-                  </p>
+                <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black opacity-60">
+                    <h2 className="text-xl text-white">{event.title}</h2>
                 </div>
-              ))}
-              {event.prices.map((price) => (
-                <div key={price.id}>
-                  <p>
-                    <strong>Price:</strong> ${price.price}
-                  </p>
-                </div>
-              ))}
+
+                {/* List tickets and prices */}
+                {event.tickets.map((ticket) => (
+                    <div key={ticket.id} className="hidden">
+                        <p>
+                            <strong>Ticket ID:</strong> {ticket.id}
+                        </p>
+                    </div>
+                ))}
             </Link>
           ))}
         </div>
--- app/routes/events.new.tsx
+++ app/routes/events.new.tsx
@@ -46,7 +46,7 @@ export default function NewEventForm() {
 
   return (
     <div className=" flex min-h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
-          <div className="blurred-background lg:h-[1000vh]"
+          <div className="blurred-background lg:h-[100vh]"
               style={{
                       backgroundImage: `url(${"/image-nightlife.jpg"})`,
                     }}
